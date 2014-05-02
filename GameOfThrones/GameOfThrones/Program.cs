183912959dba3a6a3fcb88fd78e977058580d437
Nick Gandy <ngandy1990@gmail.com>
2014-05-02 15:18:10 -0400

    Added 2 new projects
    GameOfThones: this is for our business object(pieces, board, etc)
    GameOfThrones_UI: references the project above and adds a UI layor

---
+++ GameOfThrones/GameOfThrones/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameOfThrones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
